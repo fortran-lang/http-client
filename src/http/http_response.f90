175a72c910bbea1e902ce3cb8d6f2a23fdea5b9e
milancurcic <caomaco@gmail.com>
2023-06-01 09:20:51 -0400

    Remove unneeded public attributes

--- src/http/http_response.f90
+++ src/http/http_response.f90
@@ -9,10 +9,10 @@ module http_response
 
     ! Response Type
     type :: response_type
-        character(len=:), public, allocatable :: url, content, method, err_msg, header_string
-        integer, public :: status_code = 0
-        integer(kind=int64), public :: content_length = 0
-        logical, public :: ok = .true.
+        character(len=:), allocatable :: url, content, method, err_msg, header_string
+        integer :: status_code = 0
+        integer(kind=int64) :: content_length = 0
+        logical :: ok = .true.
         type(fhash_tbl_t) :: header
     end type response_type
 
