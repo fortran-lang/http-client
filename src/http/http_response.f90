2bee99bb9c55af80fa2dbd0e24a556ece0470d15
Milan Curcic <caomaco@gmail.com>
2023-06-01 09:21:36 -0400

    `c_long` -> `c_size_t` for `content_length` (#12)
    * c_long -> c_size_t
    
    * Use int64 for content_length
    
    * Remove unneeded public attributes

--- src/http/http_response.f90
+++ src/http/http_response.f90
@@ -1,5 +1,5 @@
 module http_response
-    use, intrinsic :: iso_c_binding, only : c_size_t
+    use, intrinsic :: iso_fortran_env, only: int64
     use fhash, only: fhash_tbl_t, key => fhash_key
 
     implicit none
@@ -9,10 +9,10 @@ module http_response
 
     ! Response Type
     type :: response_type
-        character(len=:), public, allocatable :: url, content, method, err_msg, header_string
-        integer, public :: status_code = 0
-        integer(kind=c_long), public :: content_length = 0
-        logical, public :: ok = .true.
+        character(len=:), allocatable :: url, content, method, err_msg, header_string
+        integer :: status_code = 0
+        integer(kind=int64) :: content_length = 0
+        logical :: ok = .true.
         type(fhash_tbl_t) :: header
     end type response_type
 
