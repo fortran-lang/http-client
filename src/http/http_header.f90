9e4196d4cdfa217312abdab31e09d2321b6db66b
milancurcic <caomaco@gmail.com>
2023-06-19 18:16:19 -0400

    Simplify http_header.f90

--- src/http/http_header.f90
+++ src/http/http_header.f90
@@ -1,14 +1,14 @@
 module http_header
-    use iso_c_binding
-    use stdlib_string_type, only: string_type, to_lower, operator(==), char
+
+    use stdlib_ascii, only: to_lower
 
     implicit none
     private
     
     public :: header_type
-    public :: get_header_value
-    public :: put_if_header_absent
     public :: append_header
+    public :: get_header_value
+    public :: header_has_key
     
     type :: header_type
         character(:), allocatable :: key, value
@@ -21,39 +21,19 @@ contains
     !  string. If there are duplicates of the key in the header array, the function returns 
     ! the value associated with the first occurrence of the key.
     pure function get_header_value(header, key) result(val)
-        type(header_type), allocatable, intent(in) :: header(:)
+        type(header_type), intent(in) :: header(:)
         character(*), intent(in) :: key
         character(:), allocatable :: val
-        type(string_type) :: string_to_match
-        integer :: i
+        integer :: n
 
-        string_to_match = to_lower(string_type(key))
-        do i=1, size(header)
-            if(to_lower(string_type(header(i)%key)) == string_to_match) then
-                val = header(i)%value
+        do n = 1, size(header)
+            if (to_lower(key) == to_lower(header(n)%key)) then
+                val = header(n)%value
                 return
             end if
         end do
+
     end function get_header_value
-
-    ! This procedure adds the header only if it does not already exist 
-    ! in the current header array. It is typically used internally to 
-    ! add default headers.
-    subroutine put_if_header_absent(header, key, value)
-        type(header_type), allocatable, intent(inout) :: header(:)
-        character(*), intent(in) :: key, value
-        integer :: i
-        type(string_type) :: original_key, key_to_match 
-        
-        original_key = to_lower(string_type(key))
-        do i=1, size(header)
-            key_to_match = to_lower(string_type(header(i)%key))
-            if(key_to_match == original_key) then
-                return
-            end if
-        end do
-        call append_header(header, key, value)  
-    end subroutine put_if_header_absent
 
     ! This subroutine appends a new header_type object to the header array.
     subroutine append_header(header, key, value)
@@ -73,5 +53,25 @@ contains
             header(1) = header_type(key, value)
         end if
     end subroutine append_header
+
+    pure logical function header_has_key(header, key)
+        !! Return .true. if key is present in header, .false. otherwise.
+        !! HTTP headers are case insensitive, so values are converted to
+        !! lowercase before comparison.
+        type(header_type), intent(in) :: header(:)
+          !! Header to search for key
+        character(*), intent(in) :: key
+          !! Key to search in header
+        integer :: n
+
+        header_has_key = .false.
+        do n = 1, size(header)
+            if (to_lower(key) == to_lower(header(n)%key)) then
+                header_has_key = .true.
+                return
+            end if
+        end do
+
+    end function header_has_key
 
 end module http_header
