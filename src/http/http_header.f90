4e78bb0a4d33eaaf1c43179486b623e9dee75252
milancurcic <caomaco@gmail.com>
2023-06-19 18:28:31 -0400

    Add FORD-style dosctrings; order procedure definitions alphabetically

--- src/http/http_header.f90
+++ src/http/http_header.f90
@@ -1,5 +1,10 @@
 module http_header
 
+    !! This module provides a simple key value type to use for HTTP headers.
+    !! It also provides procedures to inquire about the presence of a key and
+    !! its value in a header array, as well as a procedure to append new
+    !! headers to an existing array of headers.
+
     use stdlib_ascii, only: to_lower
 
     implicit none
@@ -15,30 +20,16 @@ module http_header
     end type header_type
 
 contains
-    ! The get_header_value function takes a key string as input and returns the corresponding 
-    ! value as a string from a response_type object's header array, which contains key-value 
-    ! pairs representing HTTP headers. If the key is not found, the function returns an empty
-    !  string. If there are duplicates of the key in the header array, the function returns 
-    ! the value associated with the first occurrence of the key.
-    pure function get_header_value(header, key) result(val)
-        type(header_type), intent(in) :: header(:)
-        character(*), intent(in) :: key
-        character(:), allocatable :: val
-        integer :: n
 
-        do n = 1, size(header)
-            if (to_lower(key) == to_lower(header(n)%key)) then
-                val = header(n)%value
-                return
-            end if
-        end do
-
-    end function get_header_value
-
-    ! This subroutine appends a new header_type object to the header array.
     subroutine append_header(header, key, value)
+        !! Append a new header_type instance with key and value members to the
+        !! header array.
         type(header_type), allocatable, intent(inout) :: header(:)
-        character(*), intent(in) :: key, value
+            !! Header array to append to
+        character(*), intent(in) :: key
+            !! Key member of header_type to append
+        character(*), intent(in) :: value
+            !! Value member of header_type to append
         type(header_type), allocatable :: temp(:)
         integer :: n
 
@@ -54,14 +45,36 @@ contains
         end if
     end subroutine append_header
 
+    pure function get_header_value(header, key) result(val)
+        !! Return the value of a requested key in a header array. If the key is
+        !! not found, the function returns an empty string (unallocated). If
+        !! there are duplicates of the key in the header array, return the value
+        !! of the first occurence of the key.
+        type(header_type), intent(in) :: header(:)
+            !! Header to search for key
+        character(*), intent(in) :: key
+            !! Key to search in header
+        character(:), allocatable :: val
+            !! Value of the key to return
+        integer :: n
+
+        do n = 1, size(header)
+            if (to_lower(key) == to_lower(header(n)%key)) then
+                val = header(n)%value
+                return
+            end if
+        end do
+
+    end function get_header_value
+
     pure logical function header_has_key(header, key)
         !! Return .true. if key is present in header, .false. otherwise.
         !! HTTP headers are case insensitive, so values are converted to
         !! lowercase before comparison.
         type(header_type), intent(in) :: header(:)
-          !! Header to search for key
+            !! Header to search for key
         character(*), intent(in) :: key
-          !! Key to search in header
+            !! Key to search in header
         integer :: n
 
         header_has_key = .false.
