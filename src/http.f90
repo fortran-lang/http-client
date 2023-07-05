2da5256ea194213e2787b66f49a85df84e17f976
Rajkumar <rajkumardongre17@gmail.com>
2023-07-05 14:41:15 +0530

    add HTTP_PUT

--- src/http.f90
+++ src/http.f90
@@ -1,6 +1,6 @@
 module http
     use http_request, only: &
-        HTTP_DELETE, HTTP_GET, HTTP_HEAD, HTTP_PATCH, HTTP_POST, HTTP_POST
+        HTTP_DELETE, HTTP_GET, HTTP_HEAD, HTTP_PATCH, HTTP_POST, HTTP_PUT
     use http_response, only: response_type
     use http_client, only: request
     use http_pair, only : pair_type
